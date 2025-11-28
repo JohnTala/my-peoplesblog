32b2487a0f0743919a94fbf6330adbb997e66f62
JohnTala <57272492+JohnTala@users.noreply.github.com>
2025-11-29 00:20:58 +0200

    last debug on vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite';
 
-// Detect if we are in production (build) or dev
+// Detect if we are deploying to GitHub Pages
 const isGitHubPages = process.env.DEPLOY === 'true';
 
 export default defineConfig({
   root: 'src',
-  base: isGitHubPages ? '/my-peoplesblog/' : './', // dynamic base
+  base: isGitHubPages ? '/my-peoplesblog/' : './',  // relative locally, absolute for GH Pages
   server: {
-    port: 3000, // dev server port
+    port: 3000,  // dev server port
+  },
+  preview: {
+    port: 3000,  // preview server port
   },
   build: {
     outDir: '../dist',
